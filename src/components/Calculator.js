f8c9a298fc16d6ce8d0a3ca00f7390b9bd1404a5
ItsNekoRx28 <nicola.carambia@hotmail.com>
2023-06-10 23:16:42 +0200

    First Calculator Version

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,54 @@
+import { useState } from "react"
 import React from 'react'
 
 export const Calculator = () => {
-  return (
-    <div>Calculator</div>
-  )
+
+    const [resultado, setResultado] = useState(0);
+    const [num1, setNum1] = useState('');
+    const [num2, setNum2] = useState('');
+    const [op, setOp] = useState("+");
+
+    const calcular = (num1,num2,op) => {
+        setNum1(num1);
+        setNum2(num2);
+        setOp(op);
+        let res;
+        switch (op) {
+            case '+':
+                res = Number(num1) + Number(num2);
+                break;
+            case '-':
+                res = Number(num1) - Number(num2);
+                break;
+            case '*':
+                res = Number(num1) * Number(num2);
+                break;
+            case '/':
+                res = Number(num1) / Number(num2);
+                break;
+            case '^':
+                res = Math.pow(Number(num1), Number(num2));
+                break;
+            default:
+                res = null;
+        }
+        setResultado(res);
+    }
+
+    return (
+        <div>
+            <h1>Calculadora React</h1>
+            <input type="number" onChange={e => calcular(e.target.value, num2, op)} placeholder="0"/>
+            <select onChange={e => calcular(num1, num2, e.target.value)} placeholder="+">
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+                <option value="/">/</option>
+                <option value="^">^</option>
+            </select>
+            <input type="number" onChange={e => calcular(num1, e.target.value, op)} placeholder="0"/>
+            <br/>
+            <p>Resultado: {resultado}</p>
+        </div>
+    )
 }
